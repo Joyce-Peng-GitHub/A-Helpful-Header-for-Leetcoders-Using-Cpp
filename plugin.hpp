86343fd64799a76f8704567a87f7c302b44977a2
Joyce-Peng-GitHub <72368500+Joyce-Peng-GitHub@users.noreply.github.com>
2021-04-25 20:08:24 +0800

    Upload file plugin.hpp
    Add an operator+(const vector<T> &, const vector<T> &); .
    Revise and add some notes.
    
    新增一个运算符 operator+(const vector<T> &, const vector<T> &); 。
    修改并添加了一些注释。

--- plugin.hpp
+++ plugin.hpp
@@ -1,8 +1,8 @@
 /**
- * @name : plugin.hpp
- * @author : Joyce-Peng-GitHub
- * @version : 1.0
- * @date : 4-17-2021
+ * @file plugin.hpp
+ * @version 1.1
+ * @date 4.25.2021 19:58
+ * @author Joyce-Peng-GitHub
  */
 
 #ifndef PLUGIN_H_INCLUDED
@@ -11,8 +11,18 @@
 #include <ostream>
 #include <vector>
 
-namespace lc // "lc" refers to "Leetcode"
+/**
+ * @brief Some frequently-used class in Leetcode
+ * @details Namespace "lc" as "leetcode"
+ * 
+ * @since @version 1.0
+ */
+namespace lc
 {
+    /**
+     * @brief ListNode in Leetcode
+     * @details
+     */
     class ListNode
     {
     public:
@@ -22,6 +32,10 @@ namespace lc // "lc" refers to "Leetcode"
         ListNode(int x) : val(x), next(nullptr) {}
         ListNode(int x, ListNode *next) : val(x), next(next) {}
     };
+    /**
+     * @brief TreeNode in Leetcode
+     * @details
+     */
     class TreeNode
     {
     public:
@@ -31,12 +45,21 @@ namespace lc // "lc" refers to "Leetcode"
         TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
         TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
     };
-} // Namespace lc (Leetcode)
+} // namespace lc
 
 namespace std
 {
     ostream &operator<<(ostream &os, const vector<char> &arr)
     {
+        /**
+         * @param os The target output stream
+         * @param arr The vector array to output
+         * 
+         * @todo Output the elements of @c arr one by one,
+         *       with  a ", " between each 2 characters.
+         * 
+         * @return @c os
+         */
         if (arr.size())
         {
             os << '[';
@@ -55,6 +78,16 @@ namespace std
     }
     ostream &operator<<(ostream &os, const vector<char *> &s)
     {
+        /**
+         * @param os The target output stream
+         * @param s The strings to output
+         * 
+         * @todo Output the strings of @c s one by one,
+         *       with a " \" " on each side of every string
+         *       and a ", " between each 2 strings
+         * 
+         * @return @c os
+         */
         if (s.size())
         {
             os << '[';
@@ -73,6 +106,16 @@ namespace std
     }
     ostream &operator<<(ostream &os, const vector<string> &s)
     {
+        /**
+         * @param os The target output stream
+         * @param s The strings to output
+         * 
+         * @todo Output the strings in @c s one by one,
+         *       with a " \" " on each side of every string
+         *       and a ", " between each 2 strings
+         * 
+         * @return @c os
+         */
         if (s.size())
         {
             os << '[';
@@ -89,27 +132,18 @@ namespace std
         }
         return os;
     }
-    template <typename T>
-    ostream &operator<<(ostream &os, const vector<T> &arr)
-    {
-        if (arr.size())
-        {
-            os << '[';
-            for (int i = 0; i < arr.size() - 1; i++)
-            {
-                os << arr[i] << ", ";
-            }
-            os << arr.back() << ']';
-        }
-        else
-        {
-            os << "null";
-        }
-        return os;
-    }
 
     ostream &operator<<(ostream &os, const lc::ListNode *head)
     {
+        /**
+         * @param os The target output stream
+         * @param s The list to output
+         * 
+         * @todo Output the nodes in @c head one by one,
+         *       with a "->" between each 2 nodes
+         * 
+         * @return @c os
+         */
         if (head)
         {
             for (; head; head = head->next)
@@ -119,6 +153,27 @@ namespace std
         }
         os << "null";
         return os;
+    }
+
+    template <typename T>
+    const vector<T> operator+(const vector<T> &a, const vector<T> &b)
+    {
+        /**
+         * @todo link @c a and @c b
+         * 
+         * @return return the result with a const vector
+         */
+        vector<T> ret = vector<T>(a.size() + b.size());
+        int i = 0;
+        for (; i < a.size(); i++)
+        {
+            ret[i] = a[i];
+        }
+        for (; i < a.size() + b.size(); i++)
+        {
+            ret[i] = b[i - a.size()];
+        }
+        return ret;
     }
 }
 
